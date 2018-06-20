c0f81f4235b43ec2fb4ac60114ccee045f9b00f0
Maciej Drahusz <maciejdrahusz@gmail.com>
2018-06-20 13:44:21 +0200

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-	apiKey: "AIzaSyDWd8aSadNgGSW7goz5I8iDpkpk4i4F-vM",
+apiKey: "AIzaSyDWd8aSadNgGSW7goz5I8iDpkpk4i4F-vM",
     authDomain: "fastpack-1515648279099.firebaseapp.com",
     databaseURL: "https://fastpack-1515648279099.firebaseio.com",
     projectId: "fastpack-1515648279099",
